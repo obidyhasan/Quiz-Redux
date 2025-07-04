4e356ea95509bbf856002013e2a49c3178cff879
obidyhasan <obidyhasan@gmail.com>
2025-07-04 17:52:26 +0600

    deploy vercel

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,10 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
-import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <Toaster />
       <App />
     </Provider>
   </StrictMode>
